4fc793373ab4ec3bc92f0c45c314663df7864d47
Jaesung <bbbjason@naver.com>
2022-12-12 23:13:23 +0700

    Add toggle

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 // 네브바 클릭시 화면 이동
 
-const menu = document.querySelector('.menu');
+const menu = document.querySelector(".menu");
 
-menu.addEventListener('click',(event)=>{
- const link = event.target.dataset.link;
- const linkPosition = document.querySelector(link);
- if(link == null){
+menu.addEventListener("click", (event) => {
+  const link = event.target.dataset.link;
+  const linkPosition = document.querySelector(link);
+  if (link == null) {
     return;
-}  
-else{        
-    linkPosition.scrollIntoView({behavior: "smooth"});
-} 
+  } else {
+    linkPosition.scrollIntoView({ behavior: "smooth" });
+  }
+});
 
+// 토글 버튼
+const navbarToggleBtn = document.querySelector(".navbar_toggle_btn");
+
+navbarToggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
 });
